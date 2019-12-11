7b1c985addf0396435413e0f244fc042ca1d1ef4
Lutz <lutz.l.burning@gmail.com>
2019-12-11 19:02:05 +0800

    export navigation functions

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ import invariant from "invariant"
 import { pathToRegexp } from "path-to-regexp"
 
 const RouterContext = React.createContext()
+const history = createBrowserHistory()
+
+export const push = R.curry((rwHistory, to) => {
+  const { next } = U.destructure(rwHistory)
+
+  const url = new URL(to, window.location.href)
+  // the origin should be the same if `to` is a relative path, and History API
+  // only support relative path securely.
+  invariant(url.origin === window.location.origin, "The `to` property should be a relative path.")
+
+  next.set({
+    ...R.pick(["pathname", "search", "hash"], url),
+    type: "PUSH",
+  })
+})
+
+export const goBack = () => history.goBack()
 
 export function Link({
   to,
@@ -38,11 +55,6 @@ export function Link({
     ),
   )
 
-  const url = new URL(to, window.location.href)
-  // the origin should be the same if `to` is a relative path, and History API
-  // only support relative path securely.
-  invariant(url.origin === window.location.origin, "The `to` property should be a relative path.")
-
   return (
     <a
       href={to}
@@ -55,12 +67,7 @@ export function Link({
       onClick={U.actions(
         U.stopPropagation,
         U.preventDefault,
-        U.doSet(next,
-          {
-            ...R.pick(["pathname", "search", "hash"], url),
-            type: "PUSH",
-          }
-        ),
+        () => push(rwHistory, to),
       )}
     >
       { children }
@@ -77,7 +84,6 @@ export function Router({
   routes,
   ParentComponent,
   rwHistory = U.atom(),
-  history = createBrowserHistory(),
 }) {
   // inject transformed paths
   routes = U.thru(
